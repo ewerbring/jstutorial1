7f89b34e4e01240e489cfe1e442962cbbd7dbe14
Carl-Gustaf <ewerbring@gmail.com>
2020-01-21 12:05:50 -0500

    initial

---
+++ main.js
@@ -0,0 +1,32 @@
+///exercise 1: 
+///add 1 to the counter every time the funciton is called
+///console.log your counter variable
+
+var counter1 = 0;
+function ourCounter() {
+  ///your code here
+}
+
+///exercise 2: fill the array with ten numbers. print the numbers in the console
+///create a button in your html to call the function testing()
+var numbers = [
+
+];
+
+function testing() {
+
+    ///write a for loop that iterates over your array and for each step console.log the number
+
+}
+
+
+// exercise 3: declare a variable called counter2
+// add 5 to the counter every time the function is called
+// assign the variable to the font size
+// create a button in your html to call the function
+
+function fontSize (){
+    ///add to the counter
+    document.getElementById("header").style.fontSize = ;//assign the counters value here
+}
+
